ee9ecd73076438d03bef27026991f12b0b517746
mozzbeatt <112703642+mozzbeatt@users.noreply.github.com>
2022-09-02 23:36:02 +0930

    2
    2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `One wallet. Zero worries.`,
+    description: `The ultimate fiat-friendly, non-custodial wallet that lets you buy digital assets at fixed conversion rates.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
