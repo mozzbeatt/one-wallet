f12be6d2dc5c87918bcbb4ce393c6967d4659a5f
mozzbeatt <112703642+mozzbeatt@users.noreply.github.com>
2022-09-07 16:14:17 +0930

    Discord

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,23 +30,7 @@ const samplePageLinks = [
 
 const moreLinks = [
   { text: "Join us on Discord", url: "https://gatsby.dev/discord" },
-  {
-    text: "Documentation",
-    url: "https://gatsbyjs.com/docs/",
-  },
-  {
-    text: "Starters",
-    url: "https://gatsbyjs.com/starters/",
-  },
-  {
-    text: "Showcase",
-    url: "https://gatsbyjs.com/showcase/",
-  },
-  {
-    text: "Contributing",
-    url: "https://www.gatsbyjs.com/contributing/",
-  },
-  { text: "Issues", url: "https://github.com/gatsbyjs/gatsby/issues" },
+  { text: "Showcase", url: "https://gatsbyjs.com/showcase/" },
 ]
 
 const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
